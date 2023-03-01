c2009b24300539944c06db593ecbd784a56e8032
AllanBernier <allan.bernier1@gmail.com>
2023-03-01 16:18:50 +0100

    Validateurs & Bootstrap

--- AuthorMVC/src/main/java/com/m2i/AuthorMVC/controller/AuthorController.java
+++ AuthorMVC/src/main/java/com/m2i/AuthorMVC/controller/AuthorController.java
@@ -1,8 +1,11 @@
 package com.m2i.AuthorMVC.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +26,7 @@ public class AuthorController {
 		return "showView";
 	}
 	
-	@GetMapping("/update/{id}")
-	public String updateView(@PathVariable("id") int id,Model m) {
-		
-		m.addAttribute("author", aService.getById(id));
-		
-		return "updateView";
-	}
+
 	
 	@GetMapping("/create")
 	public String createView(Model m, Author author) {
@@ -37,7 +34,10 @@ public class AuthorController {
 	}
 	
 	@PostMapping("/create")
-	public String add(Model m, Author a) {
+	public String add(Model m,@Valid Author a, BindingResult result) {
+		if (result.hasErrors()) {
+			return "createView";
+		}
 		aService.createOrSave(a);
 		return "redirect:/show";
 	}
@@ -46,6 +46,14 @@ public class AuthorController {
 	public String delete(Model m, @PathVariable("id") int id) {
 		aService.delete( aService.getById(id) );
 		return "redirect:/show";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String updateView(@PathVariable("id") int id,Model m) {
+		
+		m.addAttribute("author", aService.getById(id));
+		
+		return "updateView";
 	}
 	
 	@PostMapping("/update/{id}")
--- AuthorMVC/src/main/java/com/m2i/AuthorMVC/entity/Author.java
+++ AuthorMVC/src/main/java/com/m2i/AuthorMVC/entity/Author.java
@@ -7,7 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
@@ -24,14 +27,19 @@ public class Author {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Length(max = 64, min = 4, message = "max 64, min 4 chars")
 	private String firstName;
-	
+
+	@Length(max = 64, min = 4, message = "max 64, min 4 chars")
 	private String lastName;
 	
+	@Positive(message = "Must be > 0")
 	private int age;
-	
+
+	@PastOrPresent(message = "Must be past or present date")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dateCreationCompte;
 	
+	@Positive(message = "Must be > 0")
 	private int nbFollowers;
 }
